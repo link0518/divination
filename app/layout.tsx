964d1bf5fe9f3ef503b824eeb6897d1eef737b7b
link0518 <104314575+link0518@users.noreply.github.com>
2025-05-16 21:56:01 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,11 @@ import Umami from "@/components/umami";
 import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
-  title: "AI 算卦 - 在线卜卦 GPT4 解读",
+  title: "科技拯救衍天",
   description:
     "AI 算卦 - 通过进行六次硬币的随机卜筮，生成卦象，并使用 AI 对卦象进行分析｜AI 算命、在线算命、在线算卦、周易易经64卦",
   appleWebApp: {
-    title: "AI 算卦",
+    title: "衍天宗 算卦",
   },
 };
 
