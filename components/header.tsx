bd5a2f7a4b5ec65528da1e437f36f19b7da7e46e
link0518 <104314575+link0518@users.noreply.github.com>
2025-08-07 14:12:20 +0800

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { ModeToggle } from "@/components/mode-toggle";
-import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="h-14 bg-secondary py-2 shadow">
       <div className="mx-auto flex h-full w-full items-center justify-center sm:max-w-md sm:justify-between md:max-w-2xl">
         <div className="flex gap-2 items-center">
-          <Image
-            src="/fox.svg"
-            alt="狐狸图标"
-            width={24}
-            height={24}
-            className="w-6 h-6"
-          />
-          <span>毛毛狐算卦助手</span>
+          <span className="text-2xl">🦊</span>
+          <span>毛毛狐卜卦助手</span>
         </div>
         <ModeToggle />
       </div>
